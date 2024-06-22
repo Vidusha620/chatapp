3db9c2270c4dd016dc9cc7dae3aa208f8bb6c827
Vidusha620 <hasindividusha@gmail.com>
2024-06-22 10:56:53 +0530

    Connecting folder path to the public folder

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-const {createServer} = require("http");
-const {Server} = require("socket.io");
+const path = require('path');
+const express = require('express');
 
-const httpServer = createServer();
-const socket = new Server(httpServer, {
-    cors:{
-        origin:"http://127.0.0.1:5500",
-    },
-});
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000 || process.env.PORT;
+
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
 
-socket.on('connection',(socket) => {
-    console.log(socket);
-
-    socket.on('message',(data) =>{
-        console.log(data);
-    });
-    
-    //Sending a message
-    socket.emit("message", "Hello");
-
-});
-
-httpServer.listen(3000, () =>{
-    console.log("Server is connected!")
-});
