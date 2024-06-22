06e35c597533cc7a4892e9a54b2d2e31afcd3683
Vidusha620 <hasindividusha@gmail.com>
2024-06-22 11:02:54 +0530

    connecting to the server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+server.listen(PORT, () => console.log('Server running on port ${PORT}'));
 
 
