c810eb60fad9e936880ce6df2253cabb26869342
Vidusha620 <hasindividusha@gmail.com>
2024-06-22 14:20:02 +0530

    Update

--- public/app.js
+++ public/app.js
@@ -1,27 +1,26 @@
-const chatForm = document.getElementById();
+const chatForm = document.getElementById('chatForm'); // Corrected ID
 const socket = io("http://localhost:3000");
 
-socket.on('message',message => {
+socket.on('message', (message) => {
     console.log(message);
     outputMessage(message);
 });
 
-chatForm.addEventListener('submit',(e) => {
+chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    //Get text
+    // Get text
     const msg = e.target.elements.msg.value;
 
-    //A message to the server
-    socket.emit('text',msg);
-
+    // A message to the server
+    socket.emit('text', msg);
 });
 
-function outputMessage(message){
+function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = '<p class="meta">Brad <span>9:12pm</span></p><p class="text">
-    ${message}
-    </p>';
-    document.getElementById()
+    div.innerHTML = `
+        <p class="meta">Brad <span>9:12pm</span></p>
+        <p class="text">${message}</p>`; 
+    document.querySelector('.chat-messages').appendChild(div); 
 }
